4689035e113b211e721bb51c4aceab1253460842
“Felipe <felipescherer.ads@gmail.com>
2019-12-19 21:53:17 -0300

    Projeto criado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
-function App() {
+import { Config } from "./config";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { CountryService } from "./services/CountryService";
+
+// redux
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { loadCountries } from "./redux/actions/actions";
+
+/* Pages */
+import Home from "./pages/Home/Home";
+import Detail from "./pages/Detail/Detail";
+import Favs from "./pages/Favs/Favs";
+
+/* CSS Global */
+import GlobalStyles from "./assets/styles/Global";
+
+const App = props => {
+  const { theme, loadCountries } = props;
+
+  // hook
+  useEffect(() => {
+    // Load countries
+    const countryService = new CountryService();
+
+    countryService.getAllCountriesFromApi().then(res => {
+      // console.log(res.data);
+      loadCountries(res.data);
+    });
+
+    return () => {
+      console.log("Destroy App");
+    };
+  }, [loadCountries]);
+
+  document.title = Config.PAGE_TITLE;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={{ scheme: theme.scheme }}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+          <Route
+            path="/country/:numericcode"
+            render={props => <Detail {...props} />}
+          />
+          <Route path="/favs" render={() => <Favs />} />
+        </Switch>
+      </BrowserRouter>
+      <GlobalStyles />
+    </ThemeProvider>
   );
-}
+};
 
-export default App;
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ loadCountries }, dispatch);
+
+const mapStateToProps = store => ({
+  theme: store.changeThemeReducer.theme
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
